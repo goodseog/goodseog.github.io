e32cb90ba99bbf6dd981d47dd2a4bfe2a0be0c6b
goodseog <goodseog.yoo@samsung.com>
2020-11-13 16:32:11 +0900

    Rotation WebGL2 boiler plate

---
+++ rotation/js/app.js
@@ -0,0 +1,121 @@
+let vertexShaderSource = `#version 300 es
+
+// an attribute is an input (in) to a vertex shader.
+// It will receive data from a buffer
+in vec4 a_position;
+
+// all shaders have a main function
+void main() {
+  // gl_Position is a special letiable a vertex shader
+  // is responsible for setting
+  gl_Position = a_position;
+}
+`;
+
+let fragmentShaderSource = `#version 300 es
+
+// fragment shaders don't have a default precision so we need
+// to pick one. highp is a good default. It means "high precision"
+precision highp float;
+
+// we need to declare an output for the fragment shader
+out vec4 outColor;
+
+void main() {
+  // Just set the output to a constant redish-purple
+  outColor = vec4(1, 0, 0.5, 1);
+}
+`;
+
+function createShader(gl, type, source) {
+  let shader = gl.createShader(type);
+  gl.shaderSource(shader, source);
+  gl.compileShader(shader);
+  let success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+  if (success) {
+    return shader;
+  }
+
+  console.log(gl.getShaderInfoLog(shader)); // eslint-disable-line
+  gl.deleteShader(shader);
+  return undefined;
+}
+
+function createProgram(gl, vertexShader, fragmentShader) {
+  let program = gl.createProgram();
+  gl.attachShader(program, vertexShader);
+  gl.attachShader(program, fragmentShader);
+  gl.linkProgram(program);
+  let success = gl.getProgramParameter(program, gl.LINK_STATUS);
+  if (success) {
+    return program;
+  }
+
+  console.log(gl.getProgramInfoLog(program)); // eslint-disable-line
+  gl.deleteProgram(program);
+  return undefined;
+}
+
+function main() {
+  // Get A WebGL context
+  let canvas = document.querySelector("#glcanvas");
+  let gl = canvas.getContext("webgl2");
+  if (!gl) {
+    return;
+  }
+
+  // create GLSL shaders, upload the GLSL source, compile the shaders
+  let vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
+  let fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
+
+  // Link the two shaders into a program
+  let program = createProgram(gl, vertexShader, fragmentShader);
+
+  // look up where the vertex data needs to go.
+  let positionAttributeLocation = gl.getAttribLocation(program, "a_position");
+
+  // Create a buffer and put three 2d clip space points in it
+  // Bind it to ARRAY_BUFFER (think of it as ARRAY_BUFFER = positionBuffer)
+  let positionBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+  let positions = [0, 0, 0, 0.5, 0.7, 0];
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+
+  // Create a vertex array object (attribute state)
+  // and make it the one we're currently working with
+  // Turn on the attribute
+  let vao = gl.createVertexArray();
+  gl.bindVertexArray(vao);
+  gl.enableVertexAttribArray(positionAttributeLocation);
+
+  // Tell the attribute how to get data out of positionBuffer (ARRAY_BUFFER)
+  let size = 2; // 2 components per iteration
+  let type = gl.FLOAT; // the data is 32bit floats
+  let normalize = false; // don't normalize the data
+  let stride = 0; // 0 = move forward size * sizeof(type) each iteration to get the next position
+  let offset = 0; // start at the beginning of the buffer
+  gl.vertexAttribPointer(positionAttributeLocation, size, type, normalize, stride, offset);
+
+  // Resize boiler plate
+  webglUtils.resizeCanvasToDisplaySize(gl.canvas);
+
+  // Tell WebGL how to convert from clip space to pixels
+  // Clear the canvas
+  gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);  
+  gl.clearColor(0, 0, 0, 0);
+  gl.clear(gl.COLOR_BUFFER_BIT);
+
+  // Tell it to use our program (pair of shaders)
+  // Bind the attribute/buffer set we want.
+  gl.useProgram(program);
+  gl.bindVertexArray(vao);
+
+  // draw
+  let primitiveType = gl.TRIANGLES;
+  let offset = 0;
+  let count = 3;
+  gl.drawArrays(primitiveType, offset, count);
+}
+
+main();
